74b4044a282499815b936381826fc4930e07bbb0
Ulisse Mini <ulisse.mini@gmail.com>
2019-02-22 20:20:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+## struct
+> ever want type safe data structures in pure lua? you've came to the right place!
+
+# Example
+```lua
+local struct = require "struct"
+
+person = struct {
+  name = "",
+  age  = 0,
+}
+
+-- this works
+uli = person {
+  name = "uli",
+  age = 14,
+}
+
+-- this does not
+invalid1 = person {
+  not_existing_key = "something",
+  name = "invalid",
+  age = 0,
+}
+
+-- this also fails
+invalid2 = person {}
+
+-- annd this fails
+invalid3 = person {
+	name = 21,
+	age = "name",
+}
+```
+also works with nested tables and functions (only make sure it is a function, not the parmaters)
+
+# TODO
+* Write good unit tests
---
+++ struct.lua
@@ -0,0 +1,52 @@
+local function errorf (format, ...)
+  error(string.format(format, ...))
+end
+
+local function typecheck (t, types)
+    for key, wantType in pairs(types) do
+      -- if its a table check it
+      if type(t[key]) == "table" then
+        typecheck(t[key], wantType)
+
+      elseif type(t[key]) ~= wantType then
+        if wantType == nil then
+          errorf("key '%s' is not part of the struct", key)
+        end
+
+        errorf("key '%s' want type '%s' got type '%s'", key, wantType, type(t[key]))
+      end
+    end
+end
+
+-- allTypes returns every type in a 'key = key; value = type' table.
+local function allTypes (t)
+  local types = {}
+
+  for key, val in pairs(t) do
+    if type(val) == "table" then
+      types[key] = allTypes(val)
+    else
+      types[key] = type(val)
+    end
+  end
+
+  return types
+end
+
+-- struct creates a new struct data structure from a table,
+-- it is type safe.
+local function struct (t)
+  if type(t) ~= "table" then
+    errorf("bad argument #1 to 'struct' (table expected, got %s)", type(t))
+  end
+
+  local types = allTypes(t)
+
+  -- return a function that typechecks the table it gets.
+  return function(t)
+    typecheck(t, types)
+    return t
+  end
+end
+
+return struct
